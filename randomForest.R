faacb4fe21584ca57ab37f6e8598693c480030e7
Nick Walker <nickwalker037@gmail.com>
2017-09-21 20:38:21 -0400

    Create randomForest.R

---
+++ randomForest.R
@@ -0,0 +1,19 @@
+library(readr)
+library(class)
+library(randomForest)
+
+train <- read_csv("/Users/nwalker018/Desktop/Datasets/Digit Recognizer/Digit_Recognizer_Train.csv")
+test <- read_csv("/Users/nwalker018/Desktop/Datasets/Digit Recognizer/Digit_Recognizer_Test.csv")
+
+nrow(train)
+ncol(train)
+labels <- factor(train[nrow(train),1])
+numTrees = 15
+
+rFor <- randomForest(train[,-1],train$label,xtest=test,ntree=numTrees)
+
+submission <- data.frame(ImageId=1:nrow(test),Label=round(rFor$test$predicted))
+
+head(submission)
+
+write.csv(submission, "/Users/nwalker018/Desktop/Datasets/Digit Recognizer/randomForest_1.csv")
